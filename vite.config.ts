3d287232ad62c77ec93a9bb1e01eb7e152f00947
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2024-03-02 18:07:57 -0800

    Change to library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,23 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import glsl from 'vite-plugin-glsl';
+
 export default defineConfig({
-  plugins: [glsl()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.ts'),
+      name: 'chamanti',
+      fileName: 'chamanti',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      output: {
+        globals: {
+          chamanti: 'Chamanti',
+        },
+      },
+    },
+  },
+  plugins: [glsl(), dts({ rollupTypes: true })],
 });
